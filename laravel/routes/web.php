29bd2100c01108b28a5c79fa78da9601fe144bed
Oscaar19 <osbude@fp.insjoaquimmir.cat>
2022-10-18 17:07:28 +0200

    Todo funcionando hasta el punto 4.2

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\MailController;
 
 /*
@@ -14,13 +13,17 @@ use App\Http\Controllers\MailController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-   Log::info('Loading welcome page');
-   Debugbar::debug('Laravel Debugbar test');
-   return view('welcome');
-});
 // ...
 Route::get('mail/test', [MailController::class, 'test']);
 // or
 // Route::get('mail/test', 'App\Http\Controllers\MailController@test');
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
