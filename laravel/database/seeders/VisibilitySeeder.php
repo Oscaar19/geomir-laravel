64dca99ca971f75fc102b68d2ab6538b3e3bec8d
Oscaar19 <osbude@fp.insjoaquimmir.cat>
2022-11-09 19:15:18 +0100

    Backpack añadido y CRUD de visibilities creado

---
+++ laravel/database/seeders/VisibilitySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VisibilitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $visibilities=[
+            // ['id' => '1', 'name' => 'author'],
+            // ['id' => '2', 'name' => 'editor'],
+            // ['id' => '3', 'name' => 'admin'],
+            ['name' => 'public'],
+            ['name' => 'contacts'],
+            ['name' => 'private'],
+        ];
+        DB::table('visibilities')->insert($visibilities);
+    }
+}
