33b2b656d5ff9165616a37ea1600e33fccf681b3
Oscar Buitrago de la Cruz <oscarbuitragodelacruz@gmail.com>
2022-11-21 13:30:19 +0100

    Diseño de la pagina home

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="htmlApp" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="maxWidthMaxHeight" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,59 +18,29 @@
 </head>
 <body class="appBody">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light backgroundNav shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+        <nav class="navFijo">
+            <div class="logoGeomir centrar">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    @include('partials.language-switcher')
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        
-                        <!-- Authentication Links -->
-                        @guest
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                    <span class="navbar-toggler-icon">GEOMIR</span>
+                </button>                
+            </div>
+            <div id="idiomsButton">
+                @include('partials.language-switcher')                    
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
+        <main class="mainHome">
+            <div id="welcomeMessage">
+                <p class="homeMessage">Discover your friend's location and their favourite places!</p>
+            </div>
+            <div id="formulario">
+                <div class="maxWidthMaxHeight">
+                    @yield('content')
+                    <li class="noDotList">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                </div>
+            </div>            
         </main>
     </div>
 </body>
