9adb098085acb0a682cd91c81564f3734f3a50cd
Oscaar19 <osbude@fp.insjoaquimmir.cat>
2022-11-24 20:18:12 +0100

    index de places maquetado

--- laravel/resources/views/places/index.blade.php
+++ laravel/resources/views/places/index.blade.php
@@ -2,43 +2,26 @@
  
 @section('content')
 <div class="container">
-   <div class="row justify-content-center">
-       <div class="col-md-8">
-           <div class="card">
-               <div class="card-header">{{ __('Places') }}</div>
-               <div class="card-body">
-                   <table class="table">
-                       <thead>
-                           <tr>
-                                <td scope="col">Name</td>
-                                <td scope="col">Description</td>
-                                <td scope="col">Latitude</td>
-                                <td scope="col">Longitude</td>
-                                <td scope="col">Created</td>
-                           </tr>
-                       </thead>
-                       <tbody>
-                           @foreach ($places as $place)
-                                <tr>
-                                    <td><a href="{{ route('places.show',$place) }}">{{ $place->name }}</a></td>
-                                    <td>{{ $place->description }}</td>
-                                    <td>{{ $place->latitude }}</td>
-                                    <td>{{ $place->longitude }}</td>
-                                    <td>{{ $place->created_at }}</td>
-                                </tr>
-                           @endforeach
-                       </tbody>
-                   </table>
-                   <form action="{{ route('places.favourite',$place) }}" method="post">
-                       @csrf 
-                       <tr>
-                           <button class="btn btn-primary">Favourite</button>
-                       </tr>
-                   </form>
-                   <a class="btn btn-primary" href="{{ route('places.create') }}" role="button">Add new place</a>
-               </div>
-           </div>
-       </div>
-   </div>
+    <div id="addPlace">
+        <a class="btn btn-primary" href="{{ route('places.create') }}" role="button">Add new place</a>
+    </div>
+    @foreach ($places as $place)
+        <div class="divPlace">
+            <tr>
+                <td><a href="{{ route('places.show',$place) }}">{{ $place->name }}</a></td>
+                <td>{{ $place->description }}</td>
+                <td>{{ $place->latitude }}</td>
+                <td>{{ $place->longitude }}</td>
+                <td>{{ $place->created_at }}</td>
+            </tr>
+            <form action="{{ route('places.favourite',$place) }}" method="post">
+                @csrf 
+                <tr>
+                    <button class="btn btn-primary">Add to favourite</button>
+                </tr>
+            </form>
+        </div>
+    @endforeach
+   
 </div>
 @endsection
--- laravel/resources/views/places/show.blade.php
+++ laravel/resources/views/places/show.blade.php
@@ -2,50 +2,42 @@
  
 @section('content')
 <div class="container">
-   <div class="row justify-content-center">
-       <div class="col-md-8">
-           <div class="card">
-               <div class="card-header">{{ __('Place') }}</div>
-               <div class="card-body">
-                   <table class="table">
-                       <thead>
-                           <tr> 
-                               <td scope="col">ID</td>
-                               <td scope="col">Name</td>
-                               <td scope="col">Description</td>
-                               <td scope="col">Latitude</td>
-                               <td scope="col">Longitude</td>
-                               <td scope="col">Author</td>
-                               <td scope="col">Created</td>
-                           </tr>
-                       </thead>
-                       <tbody>                           
-                           <tr>
-                                <td>{{ $place->id }}</td>
-                                <td>{{ $place->name }}</td>
-                                <td>{{ $place->description }}</td>
-                                <td>{{ $place->latitude }}</td>
-                                <td>{{ $place->longitude }}</td>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $place->created_at }}</td>
-                           </tr>                           
-                       </tbody>
-                   </table>
-                   <img class="img-fluid" src="{{ asset("storage/{$file->filepath}") }}" />
-                </div>
-                <div>
-                    <a class="btn btn-primary" href="{{ route('places.edit',$place) }}">Edita</a>
-                    <form method="post" action="{{ route('places.destroy',$place) }}" enctype="multipart/form-data">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-secondary">Eliminar</button>
-                    </form>
-                    <a class="btn btn-primary" href="{{ route('places.index') }}">Veure fitxers</a>
-                </div>
-                @include('flash')
-
-            </div>
-       </div>
-   </div>
+    <div class="">
+        <table class="table">
+            <thead>
+                <tr> 
+                    <td scope="col">ID</td>
+                    <td scope="col">Name</td>
+                    <td scope="col">Description</td>
+                    <td scope="col">Latitude</td>
+                    <td scope="col">Longitude</td>
+                    <td scope="col">Author</td>
+                    <td scope="col">Created</td>
+                </tr>
+            </thead>
+            <tbody>                           
+                <tr>
+                    <td>{{ $place->id }}</td>
+                    <td>{{ $place->name }}</td>
+                    <td>{{ $place->description }}</td>
+                    <td>{{ $place->latitude }}</td>
+                    <td>{{ $place->longitude }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $place->created_at }}</td>
+                </tr>                           
+            </tbody>
+        </table>
+        <img class="imgShow" src="{{ asset("storage/{$file->filepath}") }}" />
+    </div>
+    <div>
+        <a class="btn btn-primary" href="{{ route('places.edit',$place) }}">Edita</a>
+        <form method="post" action="{{ route('places.destroy',$place) }}" enctype="multipart/form-data">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-secondary">Eliminar</button>
+        </form>
+        <a class="btn btn-primary" href="{{ route('places.index') }}">Veure fitxers</a>
+    </div>
+    @include('flash')
 </div>
 @endsection
