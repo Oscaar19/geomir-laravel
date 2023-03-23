573bfb4d35b28a83dcb3f3ba4125d4b934e3dec0
Oscar Buitrago de la Cruz <osbude@fp.insjoaquimmir.cat>
2023-03-23 19:23:54 +0100

    Control por voz y informe de accesibilidad añadido

--- laravel/resources/views/contact-page.blade.php
+++ laravel/resources/views/contact-page.blade.php
@@ -17,6 +17,9 @@
 </style>
 <script src="https://cdn.jsdelivr.net/npm/ol@v7.2.2/dist/ol.js"></script>
 <div class="contacto-flex">
+    <div id="speech-div">
+        <i id="micro" class="bi bi-mic"></i>
+    </div>
     <div class="contenedor-fondo">
         <video src="./audio/video-fondo.mp4" class="video" autoplay="true" muted="true" loop="true"></video>
         <div class="texto-arriba">Contacta amb nosaltres</div>
@@ -99,6 +102,14 @@
                     <a href="https://www.witch.com"><i class="bi bi-twitch"></i></a>
                 </div>
             </div>
+            <!-- Icono de accesibilidad -->
+            <a href="https://www.w3.org/WAI/WCAG2A-Conformance"
+            title="Explanation of WCAG 2 Level A Conformance">
+                <img height="32" width="88"
+                    src="https://www.w3.org/WAI/WCAG21/wcag2.1A-v"
+                    alt="Level A conformance,
+                            W3C WAI Web Content Accessibility Guidelines 2.1">
+            </a>
         </div>
     </footer>
 </div>
@@ -108,6 +119,87 @@
     function showPosition(position) {
         var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
         marker.bindPopup("Usted esta aquí").openPopup();
+    }
+
+    //A partir de aqui se desarrolla el speechRecognition
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+    var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+    var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+    //Aqui se recogen la lista de valores para que se reconozcan por voz despues.
+    var options = [ 'sube' , 'baja' , 'aumenta zoom', 'disminuye zoom'];
+
+    var recognition = new SpeechRecognition();
+    if (SpeechGrammarList) {
+        // Aqui se genera la lista que el speech ha de reconocer uno de los valores.
+        var speechRecognitionList = new SpeechGrammarList();
+        var grammar = '#JSGF V1.0; grammar options; public <option> = ' + options.join(' | ') + ' ;'
+        speechRecognitionList.addFromString(grammar, 1);
+        recognition.grammars = speechRecognitionList;
+    }
+
+    //Le doy ajustes al objeto como idioma, resultados y alternativas esperadas.
+    recognition.continuous = false;
+    recognition.lang = 'es-ES';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    //Aqui recojo el div que he creado.
+    const micro = document.getElementById("speech-div")
+
+    //Aqui le digo que cuando se haga click en el micro, habilite la herramienta del micro en el ordenador
+    micro.addEventListener("click", function(){
+        recognition.start();
+        console.log('Ready to receive a command.');
+    })
+
+    //Aqui analizo ya los resultados que me trae el reconocimiento.
+    recognition.onresult = function(event) {
+        var opcionRecibida = event.results[0][0].transcript;
+        console.log(opcionRecibida);
+        //Si la palabra es "sube", hago scroll hacia arriba
+        if (opcionRecibida == "sube"){
+            window.scrollBy(0,-40)
+        }
+        //Si la palabra es "baja", hago scroll hacia abajo
+        else if(opcionRecibida == "baja"){
+            window.scrollBy(0,40)
+        }
+        //Si la palabra es "aumenta zoom", aumento zoom
+        else if(opcionRecibida == "aumenta zoom"){
+            var match = document.body.style.transform.match(/scale\((.*?)\)/);
+            var zoom = match ? parseFloat(match[1]) : 1;
+            document.body.style.transform = `scale(${zoom + 0.1})`;
+        }
+        //Si la palabra es "disminuye zoom", disminuyo zoom
+        else if(opcionRecibida == "disminuye zoom"){
+            var match = document.body.style.transform.match(/scale\((.*?)\)/);
+            var zoom = match ? parseFloat(match[1]) : 1;
+            document.body.style.transform = `scale(${zoom - 0.1})`;
+        }
+    }
+
+    document.onkeyup = function(e) {
+
+        //si hago ctrl alt r, subo arriba de la pagina y zoom al 100%
+        if (e.ctrlKey && e.altKey && e.which == 82) {
+
+            window.scrollTo(0,0)
+            document.body.style.transform = 'scale(1)';
+
+        }
+    }
+    //Esta funcion detecta que cuando no hay sonido, para de escuchar con el micro
+    recognition.onspeechend = function() {
+        recognition.stop();
+    }
+    //Si la palabra no coincide con ninguna de las esperadas, avisa que no coincide
+    recognition.onnomatch = function(event) {
+        console.log("I didn't recognise that color.")
+    }
+    //Si hay algun error, salta que ha ocurrido un error con la descripcion
+    recognition.onerror = function(event) {
+        console.log('Error occurred in recognition: ' + event.error);
     }
 </script>
 @endsection
